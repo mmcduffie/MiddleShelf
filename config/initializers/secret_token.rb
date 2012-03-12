6c04887c33ffb437497d7e8053acb30b21176d23
Marcus McDuffie <whitehatpsycho@gmail.com>
2012-03-11 22:22:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiddleShelf::Application.config.secret_token = 'ddc7ba77d7766ca045a56441566bf2fc41404c25b3e1e4dd1f16b60ce404bb788c0ee8e7e8b6188d99d1d57fef1564e3d925e9f2bcbe4e71f6287e3a8d10c21e'
