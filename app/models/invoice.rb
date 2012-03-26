7f54e04d60bf6a98405d00be73e52afc06e8ba8c
Marcus McDuffie <whitehatpsycho@gmail.com>
2012-03-26 10:24:04 -0400

    added scope to Invoice for today's invoices.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,5 @@
 class Invoice < ActiveRecord::Base
   has_many :invoice_items, :dependent => :destroy
-	has_one :customer
+  has_one :customer
+  scope :todays, :conditions => ['created_at >= ?', Time.now.beginning_of_day]
 end
